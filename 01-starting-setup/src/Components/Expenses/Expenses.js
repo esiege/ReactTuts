9d035df5a1859d207022edab40c271835cf14467
esiege <esiege@gmail.com>
2022-06-27 14:21:01 -0700

    added key

--- 01-starting-setup/src/Components/Expenses/Expenses.js
+++ 01-starting-setup/src/Components/Expenses/Expenses.js
@@ -23,6 +23,7 @@ function Expenses(props) {
 
       {props.expenses.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
