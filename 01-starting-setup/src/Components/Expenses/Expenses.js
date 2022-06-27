d823e904b46221ca4e7fcf7d484621d8bb15d611
esiege <esiege@gmail.com>
2022-06-27 14:43:19 -0700

    added filter

--- 01-starting-setup/src/Components/Expenses/Expenses.js
+++ 01-starting-setup/src/Components/Expenses/Expenses.js
@@ -21,14 +21,16 @@ function Expenses(props) {
         onFilterChange={filterChangeEventHandler}
       />
 
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {props.expenses
+        .filter((expense) => expense.date.getFullYear() == filterYear)
+        .map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
     </Card>
   );
 }
